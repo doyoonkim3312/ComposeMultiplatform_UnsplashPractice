636af224fa8211874e1dac090ad92e8198a3f4bf
Doyoon Kim <doyoon3312@kakao.com>
2023-10-15 22:02:00 +0900

    FEAT::101523 Add new function for instance management.
      - Add new function for removing all existing references to trigger GC.

--- src/jvmMain/kotlin/util/InstanceProvider.kt
+++ src/jvmMain/kotlin/util/InstanceProvider.kt
@@ -8,6 +8,13 @@ import viewmodel.SearchViewModel
  * Instance Provider.
  */
 
+// Function for remove all existing references to trigger GC
+fun clearInstances() {
+    SearchViewModel.clear()
+    UnsplashRemoteSource.clear()
+    ImageRepository.clear()
+}
+
 // ViewModel
 fun searchViewModel(): SearchViewModel {
     return SearchViewModel.getInstance()
