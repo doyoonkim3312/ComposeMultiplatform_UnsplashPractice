352852efad5ab33d419b645007f3ba790e638121
Doyoon Kim <doyoon3312@kakao.com>
2023-10-15 22:11:14 +0900

    REFACTOR::101523 (ongoing) fix gitignore.

--- src/jvmMain/kotlin/SensitiveConstants.kt
+++ src/jvmMain/kotlin/SensitiveConstants.kt
@@ -2,5 +2,5 @@
  * SensitiveConstant
  */
 object SensitiveConstants {
-    const val unsplashKey: String = "ti4evev7EETa0R8nliw1t45tHI9i8SStdGos"
+    const val unsplashKey: String = "ti4evev7EETa0R8nliwsdfiwfi8SStdGos"
 }
